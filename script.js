37a8209f0367124169db7fe8a333d2dd07226899
Tom Saleeba <tom.saleeba@gmail.com>
2017-06-15 17:46:35 +0930

    bumped the default page size up to 20 so the body scrolls on initial load, fixed the random item generation to use all available names

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 angular.module('myApp', ['smart-table', 'ngTableToCsv'])
   .controller('mainCtrl', ['$scope', function ($scope) {
-    var
-      nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa'],
-      familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez']
+    var nameList = ['Pierre', 'Pol', 'Jacques', 'Robert', 'Elisa']
+    var familyName = ['Dupont', 'Germain', 'Delcourt', 'bjip', 'Menez']
+    var namesCount = nameList.length
     function createRandomItem() {
       var
-        firstName = nameList[Math.floor(Math.random() * 4)],
-        lastName = familyName[Math.floor(Math.random() * 4)],
+        firstName = nameList[Math.floor(Math.random() * namesCount)],
+        lastName = familyName[Math.floor(Math.random() * namesCount)],
         age = Math.floor(Math.random() * 100),
-        email = firstName + lastName + '@whatever.com',
+        email = firstName + lastName + '@example.com',
         balance = Math.random() * 3000
       return {
         firstName: firstName,
@@ -22,6 +22,8 @@ angular.module('myApp', ['smart-table', 'ngTableToCsv'])
     for (var j = 0; j < 50; j++) {
       $scope.displayed.push(createRandomItem())
     }
+    $scope.defaultPageSize = 20
+    $scope.itemsPerPage = $scope.defaultPageSize
   }])
   .directive('stRatio', function () {
     return {
